f0729fcc8c2f4c8f866dcc577f86b52782f9acdd
Ethan <ethanjohn.montejo@cit.edu>
2026-05-16 00:52:20 +0800

    Added setters and getters for users class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -13,4 +13,18 @@ public class User {
         this.password = password;
         this.role = role;
     }
+
+    public User() {} // Empty constructor (needed by DAO when mapping from DB)
+
+    // Getters
+    public int getId()           { return id; }
+    public String getUsername()  { return username; }
+    public String getPassword()  { return password; }
+    public String getRole()      { return role; }
+
+    // Setters
+    public void setId(int id)             { this.id = id; }
+    public void setUsername(String u)     { this.username = u; }
+    public void setPassword(String p)     { this.password = p; }
+    public void setRole(String r)         { this.role = r; }
 }
