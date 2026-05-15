ac8077299ef856e0890aa57c833a0d081c46925a
Ethan <ethanjohn.montejo@cit.edu>
2026-05-16 00:51:09 +0800

    Added constructor for users class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,16 @@
 package models;
 
 public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String role; // "TENANT" or "LANDLORD"
+
+    // Constructor
+    public User(int id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
