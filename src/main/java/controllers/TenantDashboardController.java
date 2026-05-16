c951143d30be2fcc68b5b8cfb04792e37a492a8c
chrkk <christianjake.alalim@cit.edu>
2026-05-16 22:59:20 +0800

    imported some libraries in tenantdashboardcontroller

--- src/main/java/controllers/TenantDashboardController.java
+++ src/main/java/controllers/TenantDashboardController.java
@@ -1,8 +1,14 @@
 package controllers;
 
 import app.MainApp;
+import dao.PropertyDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import models.Property;
 import models.User;
 
 public class TenantDashboardController {
