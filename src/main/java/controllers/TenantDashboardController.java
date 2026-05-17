e3f1c114c383ce60464f9925b37b1e024e78f6f0
chrkk <christianjake.alalim@cit.edu>
2026-05-18 00:01:35 +0800

    fixed bugs v4

--- src/main/java/controllers/TenantDashboardController.java
+++ src/main/java/controllers/TenantDashboardController.java
@@ -5,12 +5,15 @@ import dao.PropertyDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import models.Property;
 import models.User;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -55,6 +58,24 @@ public class TenantDashboardController {
     @FXML
     public void handleBrowseProperties() {
         MainApp.switchTo("views/PropertyList.fxml");
+    }
+
+    @FXML
+    public void handlePropertyClick() {
+        int index = propertyListView.getSelectionModel().getSelectedIndex();
+        if (index >= 0) {
+            Property selected = allProperties.get(index);
+            try {
+                FXMLLoader loader = new FXMLLoader(
+                        MainApp.class.getResource("/views/PropertyDetail.fxml"));
+                Parent root = loader.load();
+                PropertyDetailController controller = loader.getController();
+                controller.setProperty(selected);
+                MainApp.navigateTo(root);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @FXML
